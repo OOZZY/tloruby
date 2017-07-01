dcc101e71a0c66fe815a60ed52958be22f22a1f9
oozzy <oozzy.tlo@gmail.com>
2017-07-01 15:17:17 -0700

    test: Use before

--- test/statistics_test.rb
+++ test/statistics_test.rb
@@ -4,46 +4,47 @@ require 'tlo/statistics'
 EPSILON = Float::EPSILON
 
 describe Tlo::Statistics do
+  before do
+    @statistics = Tlo::Statistics::Statistics.new
+  end
+
   describe 'Statistics initialize' do
     it 'should initialize statistics' do
-      statistics = Tlo::Statistics::Statistics.new
-      assert_in_delta(0, statistics.size, EPSILON)
-      assert_nil(statistics.sum)
-      assert_nil(statistics.mean)
-      assert_nil(statistics.min)
-      assert_nil(statistics.max)
-      assert_nil(statistics.range)
-      assert_nil(statistics.variance)
-      assert_nil(statistics.stddeviation)
+      assert_in_delta(0, @statistics.size, EPSILON)
+      assert_nil(@statistics.sum)
+      assert_nil(@statistics.mean)
+      assert_nil(@statistics.min)
+      assert_nil(@statistics.max)
+      assert_nil(@statistics.range)
+      assert_nil(@statistics.variance)
+      assert_nil(@statistics.stddeviation)
     end
   end
 
   describe 'Statistics add' do
     it 'should update statistics over one call' do
-      statistics = Tlo::Statistics::Statistics.new
-      statistics.add(50.0)
-      assert_in_delta(1, statistics.size, EPSILON)
-      assert_in_delta(50, statistics.sum, EPSILON)
-      assert_in_delta(50, statistics.mean, EPSILON)
-      assert_in_delta(50, statistics.min, EPSILON)
-      assert_in_delta(50, statistics.max, EPSILON)
-      assert_in_delta(0, statistics.range, EPSILON)
-      assert_in_delta(0, statistics.variance, EPSILON)
-      assert_in_delta(0, statistics.stddeviation, EPSILON)
+      @statistics.add(50.0)
+      assert_in_delta(1, @statistics.size, EPSILON)
+      assert_in_delta(50, @statistics.sum, EPSILON)
+      assert_in_delta(50, @statistics.mean, EPSILON)
+      assert_in_delta(50, @statistics.min, EPSILON)
+      assert_in_delta(50, @statistics.max, EPSILON)
+      assert_in_delta(0, @statistics.range, EPSILON)
+      assert_in_delta(0, @statistics.variance, EPSILON)
+      assert_in_delta(0, @statistics.stddeviation, EPSILON)
     end
 
     it 'should update statistics over multiple calls' do
-      statistics = Tlo::Statistics::Statistics.new
-      statistics.add(50.0)
-      statistics.add(100.0)
-      assert_in_delta(2, statistics.size, EPSILON)
-      assert_in_delta(150, statistics.sum, EPSILON)
-      assert_in_delta(75, statistics.mean, EPSILON)
-      assert_in_delta(50, statistics.min, EPSILON)
-      assert_in_delta(100, statistics.max, EPSILON)
-      assert_in_delta(50, statistics.range, EPSILON)
-      assert_in_delta(625, statistics.variance, EPSILON)
-      assert_in_delta(25, statistics.stddeviation, EPSILON)
+      @statistics.add(50.0)
+      @statistics.add(100.0)
+      assert_in_delta(2, @statistics.size, EPSILON)
+      assert_in_delta(150, @statistics.sum, EPSILON)
+      assert_in_delta(75, @statistics.mean, EPSILON)
+      assert_in_delta(50, @statistics.min, EPSILON)
+      assert_in_delta(100, @statistics.max, EPSILON)
+      assert_in_delta(50, @statistics.range, EPSILON)
+      assert_in_delta(625, @statistics.variance, EPSILON)
+      assert_in_delta(25, @statistics.stddeviation, EPSILON)
     end
   end
 end
